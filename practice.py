d50f9c070491ff78502800e9bf031e9969ab65a5
Ian Stewart <ianstewart4outlook.com>
2020-06-29 20:34:05 -0500

    made a window with words

--- practice.py
+++ practice.py
@@ -6,3 +6,7 @@ root = Tk()
 myLabel1 = Label(root, text="Hello world!")
 myLabel2 = Label(root, text="My name is Ian!")
 
+myLabel1.grid(row=0, column=0)
+myLabel1.grid(row=1, column=0)
+
+root.mainloop()
