69be5f59280bd0378d3a8d1e4f66c67588f74968
Ian Stewart <ianstewart4@outlook.com>
2020-07-03 16:52:56 -0500

    Added a text box and gave it some functionality

--- practice.py
+++ practice.py
@@ -2,7 +2,26 @@ from tkinter import *
 
 root = Tk()
 
-#creating label widget
-myLabel1 = Label(root, text="Hello world!")
-myLabel2 = Label(root, text="My name is Ian!")
+e = Entry(root, width="50")
+e.pack()
+e.insert(0, "Enter Your Name: ")
 
+
+def myClick():
+    hello = "Hello " + e.get()
+    myLabel = Label(root, text=hello)
+    myLabel.pack()
+
+
+myButton = Button(root, text="Click Me!", padx=50, command=myClick)
+myButton.pack()
+
+# Creating label widget
+# myLabel1 = Label(root, text="Hello world!")
+# myLabel2 = Label(root, text="My name is Ian!")
+
+# # Shoving it onto the screen
+# myLabel1.grid(row=0, column=0)
+# myLabel2.grid(row=1, column=0)
+
+root.mainloop()
